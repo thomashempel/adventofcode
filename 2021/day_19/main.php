49b5d858591f8affdec3b533ca181ede6fd94f14
Thomas Hempel <thomas@scriptme.de>
2021-12-19 08:41:36 +0100

    [2021] day_19 (no solution)

---
+++ 2021/day_19/main.php
@@ -0,0 +1,28 @@
+<?php
+class Day19
+{
+
+    public function test()
+    {
+
+    }
+
+    public function part1()
+    {
+        $res = 'TBI';
+
+        echo 'Part 1: ' . $res . "\n\n";
+    }
+
+    public function part2()
+    {
+        $res = 'TBI';
+
+        echo 'Part 2: ' . $res . "\n\n";
+    }
+}
+
+$day19 = new Day19();
+$day19->test();
+$day19->part1();
+$day19->part2();
