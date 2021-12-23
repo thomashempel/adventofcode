bad75d7c15200ce2330b85240acddc7ff51b44cc
Thomas Hempel <thomas@scriptme.de>
2021-12-23 06:06:07 +0100

    [unsolved][2021] day_23

---
+++ 2021/day_23/main.php
@@ -0,0 +1,28 @@
+<?php
+class Day23
+{
+
+    public function test()
+    {
+
+    }
+
+    public function part1()
+    {
+        $res = 'TBI';
+
+        echo 'Part 1: ' . $res . "\n\n";
+    }
+
+    public function part2()
+    {
+        $res = 'TBI';
+
+        echo 'Part 2: ' . $res . "\n\n";
+    }
+}
+
+$day23 = new Day23();
+$day23->test();
+$day23->part1();
+$day23->part2();
