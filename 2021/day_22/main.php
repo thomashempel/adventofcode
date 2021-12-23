d814496f65af715d142e76abf5b6643b21dda882
Thomas Hempel <thomas@scriptme.de>
2021-12-23 06:05:59 +0100

    [unsolved][2021] day_22

---
+++ 2021/day_22/main.php
@@ -0,0 +1,28 @@
+<?php
+class Day22
+{
+
+    public function test()
+    {
+
+    }
+
+    public function part1()
+    {
+        $res = 'TBI';
+
+        echo 'Part 1: ' . $res . "\n\n";
+    }
+
+    public function part2()
+    {
+        $res = 'TBI';
+
+        echo 'Part 2: ' . $res . "\n\n";
+    }
+}
+
+$day22 = new Day22();
+$day22->test();
+$day22->part1();
+$day22->part2();
