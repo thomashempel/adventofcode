7db7d39046bdbf76d1414c853557b10c14b1bf1d
Thomas Hempel <thomas@scriptme.de>
2021-12-23 06:05:47 +0100

    [unsolved][2021] day_20

---
+++ 2021/day_20/main.php
@@ -0,0 +1,28 @@
+<?php
+class Day20
+{
+
+    public function test()
+    {
+
+    }
+
+    public function part1()
+    {
+        $res = 'TBI';
+
+        echo 'Part 1: ' . $res . "\n\n";
+    }
+
+    public function part2()
+    {
+        $res = 'TBI';
+
+        echo 'Part 2: ' . $res . "\n\n";
+    }
+}
+
+$day20 = new Day20();
+$day20->test();
+$day20->part1();
+$day20->part2();
